a73b163a66d137d9f358506894f42b2a211a7146
KimLamers <kiml_1194@hotmail.com>
2023-07-20 16:25:55 +0200

    PHP if else exercises

---
+++ PHP-if-else.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+    <?php
+    // PHP if else exercise 1
+    $a = 50;
+    $b = 10;
+    if ($a > $b) {
+        echo "Hello world!";
+    }
+
+    // PHP if else exercise 2
+    $a = 50;
+    $b = 10;
+    if ($a != $b) {
+        echo "Hello world!"; 
+    }
+
+    // PHP if else exercise 3
+    $a = 50;
+    $b = 10;
+    if ($a == $b) {
+        echo "Yes";
+    } else {
+        echo "No";
+    }
+
+    // PHP if else exercise 4
+    $a = 50;
+    $b = 10;
+    if($a == $b) {
+        echo "1";
+    } elseif($a > $b) {
+        echo "2";
+    } else {
+        echo "3";
+    }
+
+    ?>
+
+    </body>
+</html> 
