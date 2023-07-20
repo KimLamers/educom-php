73a9279b1c75807ac11782a34121c20e01f0e009
KimLamers <kiml_1194@hotmail.com>
2023-07-20 14:32:00 +0200

    PHP syntax & comments exercises + Snake special

---
+++ PHP-syntax-comments.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+    <h1>My first PHP page!</h1>
+
+    <?php
+    // PHP syntax exercise 1
+    echo "Hello world!";
+    ?>
+
+    <?php
+    // PHP syntax exercise 2
+    echo "This is PHP";
+    ?>
+
+    <?php
+    // PHP syntax exercise 3
+    // This is a single-line comment
+    ?>
+
+    <?php
+    // PHP syntax exercise 4
+    /* This is a
+    multi-line comment */
+    ?>
+
+    <?php
+    // PHP syntax exercise 5
+    echo "Hello World"; // ; <--
+    ?>
+
+
+
+
+        
+
+
+    </body>
+</html>
