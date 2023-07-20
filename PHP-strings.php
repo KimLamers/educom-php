cc32ca06e211f57da441ccbdf396e112d6950845
KimLamers <kiml_1194@hotmail.com>
2023-07-20 15:14:15 +0200

    PHP strings exercises

---
+++ PHP-strings.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+        <?php
+        // PHP strings exercise 1
+        echo strlen("Hello world!");
+
+        // PHP strings exercise 2
+        echo strrev("Hello World!");
+
+        // PHP strings exercise 3
+        $oldtxt = "Hello World!";
+        $newtxt = str_replace("World", "Dolly", $oldtxt);
+
+        ?>
+
+    </body>
+</html>
