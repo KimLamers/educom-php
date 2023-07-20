2bb024a3cf287ca18dcbe0500246751d5b8ecd26
KimLamers <kiml_1194@hotmail.com>
2023-07-20 16:40:06 +0200

    PHP switch exercises

---
+++ PHP-switch.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+    <?php
+    // PHP switch exercise 1
+    switch($color) {
+        case "red":
+            echo "Hello";
+            break;
+        case "green":
+            echo "welcome";
+            break;
+    }
+
+    // PHP switch exercise 2
+    switch ($color) {
+        case "red":
+            echo "Hello";
+            break;
+        case "green":
+            echo "Welcome";
+            break;
+        default:
+            echo "Neither";
+    }
+
+    ?>
+
+    </body>
+</html>
