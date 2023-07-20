fcd057ccb32b18694db8d0e3ec222988f86f8f7b
KimLamers <kiml_1194@hotmail.com>
2023-07-20 15:53:00 +0200

    PHP operators exercises

---
+++ PHP-operators.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+    <?php
+    // PHP operators exercise 1
+    echo 10 * 5;
+
+    // PHP operators exercise 2
+    echo 10 / 2;
+
+    // PHP operators exercise 3
+    var_dump($a == $b);
+
+    // PHO operators exercise 4
+    var_dump($a != $b);
+    ?>
+
+    </body>
+</html>
