6f78354f651bf08f2b4c4b7d9bfa0a399d0c28d8
KimLamers <kiml_1194@hotmail.com>
2023-07-20 14:55:53 +0200

    PHP variables exercises

---
+++ PHP-variables.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+    <?php
+    // PHP variables exercise 1
+    $txt = "Hello";
+
+    // PHP variables exercise 2
+    $x = 5;
+    $y = 7
+    echo $x + $y;
+
+    </body>
+</html>
