7384ed92046df724402ed4d01bd7632554e5377d
KimLamers <kiml_1194@hotmail.com>
2023-07-21 07:53:16 +0200

    PHP functions exercises

---
+++ PHP-functions.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+
+        // PHP functions exercise 1
+        function myFunction() {
+            echo "Hello world!";
+        }
+
+        // PHP functions exercise 2
+        function myFunction() {
+            echo "Hello world!";
+        }
+
+        myFunction();
+
+        // PHP functions exercise 3
+        function myFunction($fname, $lname) {
+            echo $fname;
+        }
+
+        // PHP function exercise 4
+        function myFunction($fname, $lname) {
+            return $lname;
+        }
+
+        ?>
+    </body>
+</html>
