929be3d27e95e142b70a8688d8cecf3e7fefe09b
KimLamers <kiml_1194@hotmail.com>
2023-07-21 10:14:10 +0200

    PHP dates and times exercises (#3)

---
+++ PHP-dates.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+
+        // PHP dates exercise 1
+        echo date("l");
+
+        // PHP dates exercise 2
+        echo date("Y.m.d");
+
+        // PHP dates exercise 3
+        echo date("H:i:s");
+
+        ?>
+    </body>
+</html>
